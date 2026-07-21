ac14c66aba66030aa41446f1c19aa71092127191
rentfoxxy <adminn@rentfoxxy.com>
2026-07-21 20:36:45 +0530

    inventory_lsit

--- backend/utils/inventoryListQuery.js
+++ backend/utils/inventoryListQuery.js
@@ -1,7 +1,7 @@
 /**
  * Shared SQL builder for inventory list + export (vendor_serial_numbers segments).
  */
-const { appendInventoryTagAccessFilter } = require('./inventoryTagAccessScope');
+const { appendInventoryTagAccessFilter } = require('../services/inventoryTagAccessScope');
 const { appendDateRangeClauses } = require('./dateRangeFilter');
 const { buildSerialSpecFilter } = require('./inventorySpecFilter');
 
